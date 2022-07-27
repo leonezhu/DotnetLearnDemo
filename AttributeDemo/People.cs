49f0c84bbcd6bb24defbe70ccf994778d4f9deb9
t00027924 <t00027924@vipshop.com>
2022-07-27 10:09:37 +0800

    +++

---
+++ AttributeDemo/People.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace hello1
+{
+    class People
+    {
+        int Age { set; get; }
+
+        string Name { set; get; }
+
+        public string hello()
+        {
+            return this.Name + this.Age;
+        }
+
+        public People(int Age, string Name)
+        {
+            this.Age = Age;
+            this.Name = Name;
+        }
+
+        public People()
+        {
+        }
+    }
+}
